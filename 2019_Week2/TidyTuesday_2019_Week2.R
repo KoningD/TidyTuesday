e14a36f3a5f6dc02db5ddf939acef68470e89e72
Dewi Koning <dewikoning@gmail.com>
2019-01-07 15:58:06 +0100

    Initial commit, reading in the data

---
+++ 2019_Week2/TidyTuesday_2019_Week2.R
@@ -0,0 +1,6 @@
+#TidyTuesday 2019 - Week 2
+#By: Dewi Koning
+#Github: https://github.com/KoningD
+#Twitter: https://twitter.com/DewiKoning
+
+df <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-01-08/IMDb_Economist_tv_ratings.csv")
