09415db07537dc4fd25aa2912b1668e11fb92929
Dewi Koning <dewikoning@gmail.com>
2019-06-19 18:21:05 +0200

    Extension of code with faceted plot of 3 selected birds.

--- 2019_Week25/TidyTuesday_2019_Week25.R
+++ 2019_Week25/TidyTuesday_2019_Week25.R
@@ -26,3 +26,36 @@ total_bird_counts %>% ggplot(aes(x=year, y = n)) +
 anim_save("./2019_Week25/gif_bird_counts.gif")
 
 
+#plot for selected birds 
+
+birds_more_than_100 <- bird_counts %>% group_by(species) %>% tally(how_many_counted) %>% filter(n > 10)
+
+#selection purely based on names sounding funny
+birds_selection <- c("Black-capped Chickadee", "Bufflehead", "Rock Pigeon")
+
+bird_counts_selection <- bird_counts %>% filter(species %in% birds_selection)
+
+bird_counts_selection %>% ggplot(aes(x=year, y = how_many_counted, group = species, colour = species)) + 
+  geom_line(size = 0.7) +
+  geom_point(size = 2) +
+  geom_segment(aes(xend = 2017, yend = how_many_counted), linetype = 2, colour = 'grey') + 
+  geom_text(aes(x = 2017, label = species), hjust = 0) +
+  transition_reveal(year)+
+  theme_minimal() + 
+  labs(title = "Spottings of selected birds with funny names since 1921", 
+       subtitle = "Birds heard or seen every year around Christmas in the Hamilton area of Ontario", 
+       caption = "Data: Bird Studies Canada | Hamilton area of Ontario - Plot: Dewi Koning - Tidytuesday 2019-Week 25") +
+  ylab("Number of birds spotted") + 
+  xlab("Year") +
+  facet_grid(species~.) + 
+  theme(legend.position = "none",
+        plot.margin = margin(5.5, 120, 5.5, 5.5),
+        strip.background = element_blank(), 
+        strip.text = element_blank(), 
+        plot.caption = element_text(hjust = 0)) + 
+  coord_cartesian(clip = 'off')
+
+
+anim_save("./2019_Week25/selected_birds.gif")
+
+
